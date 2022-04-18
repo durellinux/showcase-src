29a25a243e26e35ebc48ed3a14dfac4078d8b566
Gianluca Carlo Durelli <durellinux@gmail.com>
2022-04-18 09:20:26 +0200

    Added vue component library project

---
+++ showcase-component-library-vue/dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { ShowcaseComponentLibraryVueSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  ShowcaseComponentLibraryVueSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <showcase-component-library-vue-sample />
+  </div>
+</template>
---
+++ showcase-component-library-vue/src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as ShowcaseComponentLibraryVueSample } from './showcase-component-library-vue-sample.vue';
